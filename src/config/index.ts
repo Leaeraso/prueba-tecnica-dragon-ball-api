35533c032483e979f0147c929556356df7b7e2ed
Leaeraso <leaeraso@gmail.com>
2025-02-13 13:15:50 -0300

    feat/mvc+config

---
+++ src/config/index.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  throw new Error(`Couldn't find .env file`);
+}
+
+export default {
+  HTTP_PORT: Number(process.env.HTTP_PORT || 9000),
+  API_URL: process.env.API_URL || 'http://localhost',
+};
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(() => {
+  console.log(`Server running on `);
+});
